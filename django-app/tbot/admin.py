bf64ffde484bbaa5a8c99fa9cf362f899a83c012
Igor Tsupko <i@tsupko.tech>
2022-04-25 19:14:04 +0300

    Заготовка для админки

--- django-app/tbot/admin.py
+++ django-app/tbot/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from tbot.models import *
 
-# Register your models here.
+@admin.register(Estate)
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ('estate_code','users_and_weights','_total_real_space')
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('chat_id','title')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id','personal_chat_id','name','verified')
+
+
